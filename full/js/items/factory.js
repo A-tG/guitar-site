8e0fd0125450614bf9a290843eaeeb9d4615fc0d
A-tG <sappykda@gmail.com>
2018-06-10 16:21:12 +0300

    fixed small bugs

--- full/js/items/factory.js
+++ full/js/items/factory.js
@@ -3,7 +3,7 @@ itemsFactory = {
     {
         var item = {};
         var parsedArr = [];
-        var type = SCALES_TYPE;
+        var type = "";
         if (JSONstring)
         {  
             try
@@ -11,7 +11,10 @@ itemsFactory = {
                 parsedArr = JSON.parse(JSONstring);
                 type = parsedArr[0];
             } 
-            catch (err) {}
+            catch (err) 
+            {
+                console.error("Invalid JSON: " + JSONstring);
+            }
         }
         switch (type)
         {
@@ -22,6 +25,7 @@ itemsFactory = {
                 item = new ChordsItem(id, JSONstring);
                 break;
             default:
+                console.error("Invalid item type: " + type);
                 item = new ScalesItem(id);
         }
         return item;
