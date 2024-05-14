38d46915336c1f7f5d692d83049bec508a896d10
A-tG <a3tg.developer@gmail.com>
2024-05-15 00:59:45 +0300

    scale notes basic implementation

--- src/src/components/items/guitar/Scales/ScaleNoteInterval.vue
+++ src/src/components/items/guitar/Scales/ScaleNoteInterval.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
-import { ref, toRef } from 'vue';
 
 const baseWidth = 10
 const intervalsWidth = [1, 3.5, 5.5, 7, 9, 11, 13, 15]
 
-const props = defineProps <{
+defineProps <{
     number: number,
+    relToMaj: string,
     noteName: string,
-    interval: number
+    interval?: number
 }>()
-const noteName = toRef(() => props.noteName)
-const interval = toRef(() => props.interval)
-const active = ref(true)
+const isActive = defineModel<boolean>('isActive', { required: true })
 </script>
 
 <template>
-    <li class="scale-note el-clr hov-el-clr norm-bg2" @click="active = !active">
+    <li class="scale-note el-clr hov-el-clr norm-bg2" @click="isActive = !isActive">
         <span class="fnt f18 norm-clr">{{ number }}</span>
-        <span class="fnt f12 norm-clr">1</span>
-        <span class="fnt f18 tr-al" :class="active ? '' : 'transparent'">{{ noteName }}</span>
+        <span class="fnt f12 norm-clr">{{ relToMaj }}</span>
+        <span class="fnt f18 tr-al" :class="isActive ? '' : 'transparent'">{{ noteName }}</span>
         <div></div>
     </li>
-    <li class="semitone-block f14" :style="`width: ${intervalsWidth[interval - 1] * baseWidth}px`">
+    <li class="semitone-block f14" v-if="interval" :style="`width: ${intervalsWidth[interval - 1] * baseWidth}px`">
         <span class="fnt norm-clr">{{ interval }}</span>
     </li>
 </template>
