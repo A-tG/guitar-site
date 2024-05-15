ed33575ee1c53ce8897c99f103c6ee0ab392c009
A-tG <a3tg.developer@gmail.com>
2024-05-16 00:01:17 +0300

    list of names

---
+++ src/src/components/items/guitar/Scales/ScalesNames.ts
@@ -0,0 +1,47 @@
+import { type TuningID } from "@/types/Scales"
+import { ref, type Ref } from "vue"
+
+const pent = "p"
+
+const names: Record<TuningID, Ref<string>> = {
+    'maj': ref('major'),
+    'min': ref('minor'),
+    'harm_min': ref('harmonic minor'),
+    'chrom': ref('chromatic'),
+    'dor': ref('dorian'),
+    'phr': ref('phrygian'),
+    'phr_dom': ref('phrygian dominant'),
+    'lyd': ref('lydian'),
+    'mix': ref('myxolydian'),
+    'locr': ref('locrian'),
+    'mel_min': ref('melodic minor'),
+    'alt_dom': ref('altered dominant'),
+    'bl_min_hex': ref('blues minor hexatonic'),
+    'bl_pent': ref('blues ' + pent),
+    'bl': ref('blues'),
+    'maj_pent': ref('major ' + pent),
+    'sus_pent': ref('suspenden ' + pent),
+    'phr_pent': ref('phrygian ' + pent),
+    'mix_pent': ref('myxolydian ' + pent),
+    'aeol_pent': ref('aeolian ' + pent),
+    'maj_beb': ref('major bebop'),
+    'min_beb': ref('minor bebop'),
+    'dom_beb': ref('dominant bebop'),
+    'wton': ref('whole tone'),
+    'whton': ref('whole-half tone'),
+    'hwton': ref('half-whole tone'),
+    'ac': ref('acoustic'),
+    'ion': ref('ionian'),
+    'aeol': ref('aeolian'),
+    'span_mod': ref('spanish mode'),
+    'sup_locr': ref('super locrian'),
+    'dim_wton': ref('diminished whole tone'),
+    'alt_sc': ref('altered scale'),
+    'dor_pent': ref('dorian ' + pent),
+    'min_pent': ref('minor ' + pent),
+    'dor_beb': ref('dorian bebop'),
+    'dim': ref('diminished'),
+    'dom_dim': ref('dominant diminished'),
+    'symm_dim': ref('symmetrical diminished'),
+    'bart': ref('bartok')
+}
