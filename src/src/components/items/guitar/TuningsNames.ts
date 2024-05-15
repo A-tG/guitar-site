ece3526f7d286656e0a45e89b608f01a17b6cf43
A-tG <a3tg.developer@gmail.com>
2024-05-16 00:23:41 +0300

    refactoring

--- src/src/components/items/guitar/TuningsNames.ts
+++ src/src/components/items/guitar/TuningsNames.ts
@@ -1,34 +1,16 @@
-import { getTuningsIds, type TuningID } from "@/types/Tunings";
+import { type TuningID } from "@/types/Tunings";
 import { ref, type Ref } from "vue";
 
-const names = new Map<TuningID, Ref<string>>([
-    ['std_e', ref('standard e')],
-    ['std_e_bass', ref('bass standard e')],
-    ['std_e_bass6', ref('bass standard e 6')],
-    ['drop_d6', ref('drop d')],
-    ['drop_a7', ref('drop a 7')],
-    ['drop_e8', ref('drop e 8')]
-])
+const names: Record<TuningID, Ref<string>> = {
+    'std_e': ref('standard e'),
+    'std_e_bass': ref('bass standard e'),
+    'std_e_bass6': ref('bass standard e 6'),
+    'drop_d6': ref('drop d'),
+    'drop_a7': ref('drop a 7'),
+    'drop_e8': ref('drop e 8')
+}
 
 export function getTuningName(id: TuningID)
 {
-    return names.get(id) ?? ref('unknown')
+    return names[id] ?? ref('unknown')
 }
-
-function checkNames()
-{
-    const tunings = getTuningsIds()
-    if (names.size != tunings.length)
-    {
-        console.warn('Tunings ids and names arrays length mismatch')
-        return
-    }
-    for (let [k] of names)
-    {
-        if (tunings.includes(k)) continue
-
-        console.warn(`Missing ID '${k}' in tunings`)
-    }
-}
-
-checkNames()
