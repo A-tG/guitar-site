261b7385ce7b4bb2435a9ecf4a1d6c0f220ec62f
A-tG <a3tg.developer@gmail.com>
2024-05-16 17:18:50 +0300

    html minification

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,11 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createHtmlPlugin({
+      minify: true
+    })
   ],
   resolve: {
     alias: {
