9cfbcab69d394aa95f39c6eed1429c50e85ede1f
A-tG <a3tg.developer@gmail.com>
2024-05-17 04:04:46 +0300

    moved styles to vue file

--- src/src/components/settings/Settings.vue
+++ src/src/components/settings/Settings.vue
@@ -19,13 +19,13 @@ const isShown = ref(false)
 
     <SafeTeleport to="#modal" :disabled="!isShown">
         <ModalWindow title="Settings" v-show="isShown" :onCloseButton="() => isShown = false">
-            <div class="settings-content">
+            <div class="content">
                 <span class="title" :class="commonClasses">Theme</span>
                 <ThemeSwitcher></ThemeSwitcher>
-                <div class="settings-separator norm-bg2"></div>
+                <div class="separator norm-bg2"></div>
                 <span class="title" :class="commonClasses">Notation</span>
                 <NotationSwitcher></NotationSwitcher>
-                <div class="settings-separator norm-bg2"></div>
+                <div class="separator norm-bg2"></div>
                 <div class="whole-row">
                     <span class="title" :class="commonClasses">Inactive note opacity</span>
                     <InactiveNoteOpacity></InactiveNoteOpacity>
@@ -36,7 +36,25 @@ const isShown = ref(false)
 </template>
 
 <style scoped>
-.whole-row .title {
+.whole-row > .title {
     display: block;
+}
+.content {
+    display: grid;
+    grid-template-columns: auto auto;
+    align-items: center;
+    gap: 8px;
+}
+.whole-row {
+    grid-column: 1 / -1;
+}
+.content .title {
+    justify-self: left;
+    text-align: start;
+}
+.separator {
+    grid-column: 1 / -1;
+    height: 2px;
+    width: 100%;
 }
 </style>
