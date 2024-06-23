8305385fc57a22640e24f334dd372d21ab1c9bdf
A-tG <a3tg.developer@gmail.com>
2024-06-23 23:12:22 +0300

    added search for scales select

--- src/src/components/items/guitar/Scales/ScalesItem.vue
+++ src/src/components/items/guitar/Scales/ScalesItem.vue
@@ -12,7 +12,7 @@ import { NoteDisplayMode } from "../NoteDisplayMode";
 import type { ScalesItemState } from "./types/ScalesItemState";
 import type { IState } from "./types/IState";
 import SvgIcon from '@jamescoyle/vue-icon';
-import { mdiCheckboxBlankOutline, mdiCheckboxMarked } from "@mdi/js";
+import { mdiCheckboxBlankOutline, mdiCheckboxMarked, mdiMagnify, mdiBackspace } from "@mdi/js";
 
 const relToMajList = stepsRelativeToMajor
 const textCommonClass1 = "norm-clr fnt f16"
@@ -118,6 +118,8 @@ function updateNotesExtraNames()
         note = getHigherNote(note, intervals.value[i])
     }
 }
+
+const filter = ref("")
 </script>
 
 <template>
@@ -148,15 +150,39 @@ function updateNotesExtraNames()
         <div class="scale-sel-block">
             <div class="scale-sel-title">
                 <span :class="textCommonClass2">Scale</span>
+                <div class="filter-cont norm-bg3" :class="textCommonClass2">
+                    <SvgIcon class="tr-al filter-btn norm-clr" type="mdi" :size="22" :path="mdiMagnify"></SvgIcon>
+                    <input aria-label="Filter scales" class="filter-inp f18 norm-clr" type="text" 
+                        v-model="filter">
+                    <SvgIcon class="tr-al filter-btn el-clr hov-el-clr" type="mdi" :size="22"
+                        :path="mdiBackspace" @click="filter = ''" :class="{ invis: filter.length == 0}"></SvgIcon>
+                </div>
                 <div class="el-clr hov-el-clr" @click="isTriadMode = !isTriadMode">
                     <span :class="textCommonClass2">Triads mode</span>
-                    <SvgIcon class="tr-al" type="mdi" :size="24"
+                    <SvgIcon class="tr-al" type="mdi" :size="22"
                         :path="isTriadMode ? mdiCheckboxMarked : mdiCheckboxBlankOutline"></SvgIcon>
                 </div>
             </div>
             <select aria-label="Select scale" class="fnt f18 capitalized el-clr norm-bg3" v-model="selectedScale">
-                <option v-for="s in getSortedConcatNamesIDs()" :value="s">{{ getConcatScaleName(s).value }}</option>
+                <option v-for="s in getSortedConcatNamesIDs()" :value="s"
+                    v-show="(filter.trim().length == 0) || (getConcatScaleName(s).value.includes(filter) )">{{ getConcatScaleName(s).value }}</option>
             </select>
         </div>
     </div>
 </template>
+
+<style scoped>
+.filter-cont {
+    flex: 1;
+    margin: 0 15px;
+    display: flex;
+}
+.filter-btn {
+    margin-right: 5px;
+}
+.filter-inp {
+    background: none;
+    border: 0;
+    flex: 1;
+}
+</style>
