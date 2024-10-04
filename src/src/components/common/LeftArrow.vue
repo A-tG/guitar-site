bedf19c8f27279aa13071df73fd34467a50fd1d9
A-tG <a3tg.developer@gmail.com>
2024-10-04 21:55:18 +0300

    replaced border arrows with svg

--- src/src/components/common/LeftArrow.vue
+++ src/src/components/common/LeftArrow.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronLeftBox } from "@mdi/js";
+</script>
+
 <template>
-    <div class="l-arr l-arr-el-clr l-arr-el-hov tr-al"></div>
+    <SvgIcon class="el-clr hov-el-clr tr-al" type="mdi" :size="22" :path="mdiChevronLeftBox"></SvgIcon>
 </template>
--- src/src/components/common/RightArrow.vue
+++ src/src/components/common/RightArrow.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronRightBox } from "@mdi/js";
+</script>
+
 <template>
-    <div class="r-arr r-arr-el-clr r-arr-el-hov tr-al"></div>
+    <SvgIcon class="el-clr hov-el-clr tr-al" type="mdi" :size="22" :path="mdiChevronRightBox"></SvgIcon>
 </template>
